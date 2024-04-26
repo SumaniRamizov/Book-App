fa2a997d8b8d43df8b9551e8e7ca83fd1f07a527
SumaniRamizov <166131802+SumaniRamizov@users.noreply.github.com>
2024-04-26 12:55:40 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 
 import { useState } from 'react'
-import './App.css'
 import bookImg from './img/book.png'
 import { FiSearch } from "react-icons/fi";
 import Card from './components/Card';
-
-
 
 function App() {
   const [search,setSearch] = useState("")
